c8bcebc1e8c47864a0faf1f9043212cc0217481d
lahiru-j <lahirujayathilake@gmail.com>
2018-06-16 10:57:20 +0530

    call the distribution from a main method

--- gaussian-helper/src/main/java/org/airavata/datacat/helper/gaussian/DefaultGaussianParser.java
+++ gaussian-helper/src/main/java/org/airavata/datacat/helper/gaussian/DefaultGaussianParser.java
@@ -2,4 +2,14 @@ package org.airavata.datacat.helper.gaussian;
 
 public class DefaultGaussianParser {
     public final static int randomNum = (int) (Math.random() * 100000000);
+
+    public static void main(String[] args) {
+        try {
+            // Calling the distribution
+            System.out.println(Distribution.getValues(args[0]));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
