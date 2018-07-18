c3d249d3a3743be303e6bf8d9bc1df9f86330caa
lahiru-j <lahirujayathilake@gmail.com>
2018-07-18 21:52:08 +0530

    changed the working directory and limited the arguments to two

--- datacat/chars-to-lower-case/chars_to_lower_case.py
+++ datacat/chars-to-lower-case/chars_to_lower_case.py
@@ -1,13 +1,14 @@
 import sys
 from os.path import join
 
-input_file_name = sys.argv[1]       # Input file to the experiment
-working_dir = sys.argv[2]           # Working directory
-output_file_name = sys.argv[3]      # Output file name
+WORKING_DIR = "/datacat/working-dir"    # Working directory
 
-output_file = open(join(working_dir, output_file_name), 'w')
+input_file_name = sys.argv[1]           # Input file to the experiment
+output_file_name = sys.argv[2]          # Output file name
 
-with open(join(working_dir, input_file_name)) as f:
+output_file = open(join(WORKING_DIR, output_file_name), 'w')
+
+with open(join(WORKING_DIR, input_file_name)) as f:
     for line in f:
         output_file.write(line.lower())
 output_file.close()
--- datacat/txt-to-csv/txt_to_csv.py
+++ datacat/txt-to-csv/txt_to_csv.py
@@ -2,13 +2,14 @@ import sys
 import csv
 from os.path import join
 
-input_file_name = sys.argv[1]   # Input file to the experiment
-working_dir = sys.argv[2]       # Working directory
-output_file_name = sys.argv[3]  # Output file name
+WORKING_DIR = "/datacat/working-dir"    # Working directory
 
-with open(join(working_dir, input_file_name), 'r') as in_file:
+input_file_name = sys.argv[1]           # Input file to the experiment
+output_file_name = sys.argv[2]          # Output file name
+
+with open(join(WORKING_DIR, input_file_name), 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line.split(",") for line in stripped if line)
-    with open(join(working_dir, output_file_name), 'w') as out_file:
+    with open(join(WORKING_DIR, output_file_name), 'w') as out_file:
         writer = csv.writer(out_file)
         writer.writerows(lines)
